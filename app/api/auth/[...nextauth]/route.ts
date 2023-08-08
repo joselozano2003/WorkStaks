58b7305ddad7fb87e86d8f01031a949e3f772226
Jose Lozano <josecamilo.lozanocet@ucalgary.ca>
2023-08-07 18:56:42 -0600

    Auth Setup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import NextAuth from "next-auth/next";
 import { NextAuthOptions} from 'next-auth'
+
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
@@ -12,4 +17,4 @@ export const authOptions: NextAuthOptions = {
 }
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST};
+export { handler as GET, handler as POST };
---
+++ components/buttons.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+
+
+export function SignInButton(){
+    const { data: session, status } = useSession();
+
+    if (status === 'loading') {
+        return <p className="text-xl font-semibold">Loading...</p>
+    }
+
+    if (status === 'authenticated') {
+        return (
+           <></>
+        );
+    }
+
+    return <button className="btn" onClick={() => signIn()}>Sign In</button>
+}
+
+
+export function SignOutButton(){
+    return <button className="btn btn-warning ml-2 text-center" onClick={() => signOut()}>Sign Out</button>
+}
